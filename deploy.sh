5ea4ac1d095e8c723a0909e9b47bb6b5cd9f5076
Bailey Butler <bailey.butler@anz.com>
2024-02-05 11:25:22 +1100

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker build -t server --platform linux/amd64  . \
-  && docker tag server gcr.io/baileybutler-syd/sreekipedia \
-  && docker push gcr.io/baileybutler-syd/sreekipedia \
-  && gcloud run deploy sreekipedia --image gcr.io/baileybutler-syd/sreekipedia:latest --platform managed --region asia-southeast1 --allow-unauthenticated
